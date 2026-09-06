7d3cdf2a484e7b2413c4308f737869fa130d255c
Cihan Aktas <cihan.aktas@gmail.com>
2026-09-06 15:40:37 +0200

    style: resolves formatting issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { App } from "@/App";
 import "@/index.css";
 
 const rootElement = document.getElementById("app");
-if (!rootElement) {throw new Error("Root element #app not found");}
+if (!rootElement) {
+  throw new Error("Root element #app not found");
+}
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
