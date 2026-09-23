9a09ad488943177fa87a1c8ec6d637ec0b626154
Cihan Aktas <cihan.aktas@gmail.com>
2026-09-24 01:14:15 +0200

    fix: add file extension to vite config import

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { mergeConfig } from "vite";
-import viteConfig from "./vite.config";
+import viteConfig from "./vite.config.ts";
 import { defineConfig } from "vitest/config";
 
 export default mergeConfig(
