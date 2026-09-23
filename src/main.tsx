8fa6a46f116ac9732cfc84f484a6adc4e81aab46
Cihan Aktas <cihan.aktas@gmail.com>
2026-09-24 01:18:21 +0200

    chore: enable verbatimModuleSyntax and use named StrictMode import

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "@/App";
 import "@/index.css";
@@ -9,7 +9,7 @@ if (!rootElement) {
 }
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
